f6150f6aa49f6a228c536fc2a6ca9ae639625561
ikhiarabdeljalil6@gmail.com <ikhiarabdeljalil6@gmail.com>
2022-06-16 18:27:04 +0100

    test

--- app/src/main/java/com/example/youreyes/ui/theme/ListenerActivity.java
+++ app/src/main/java/com/example/youreyes/ui/theme/ListenerActivity.java
@@ -80,7 +80,7 @@ V2.setOnClickListener(new View.OnClickListener() {
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     text=  result.get(0);
-                    N=2;
+                    N=1;
                 }
                 break;
             }
